c837d41502b0f9a24d84b8775894aa5842425cbb
Bobby Atkins <bobby.timothy.atkins@gmail.com>
2022-11-18 22:55:23 -0600

    november 18th

--- js/jquery_lecture.js
+++ js/jquery_lecture.js
@@ -31,3 +31,85 @@
     });
 
 })();
+//jQuery: Selectors
+//best practice to wrap jquery in this
+//makes sure loads on the document before invoking jquery
+$(document).ready(function(){
+    // $ is jquery alias
+    // * means select "all"
+    // for style use .css
+    //innerHTML use .html
+
+    //selecting html by element
+    //jQuery('h3').html('NOT NOTICE');
+    //selecting html by id
+    //$('#first-id').html('NOT NOTICE');
+    //selecting by class
+    //$('.first-class').html('NOT NOTICE');
+
+    //selecting css by class
+    //syntax: $(class selector).css('attribute', 'value')
+    $('.important').css('background-color', 'yellow');
+    //selecting css by id
+    $('#second-id').css('font-size', '50px');
+
+    //multiple selectors
+    //$('.important, p').html("We Changed This").css('background-color', 'blue')
+    //the asterisk is the "all" selector
+    $('*').html("We Changed This").css('background-color', 'blue');
+
+    //jQuery: Mouse Events
+    //.click()
+    //.dblclick()
+    //.hover()
+
+    // the 'e' in the function parameter stands for 'event'
+    $('#codeup').click(function(e) {
+        alert('h1 with the id of "codeup" was double clicked!');
+    });
+
+
+    $('#codeup').dblclick(function(e) {
+        alert('h1 with the id of "codeup" was double clicked!');
+    });
+
+
+    // the 'this' uses the same object attached to the method
+    // in this case: this = '#codeup'
+    $('#codeup').hover(
+        function() {
+            $(this).css('background-color', 'white');
+        },
+        function() {
+            $(this).css('background-color', 'transparent');
+        }
+    );
+
+})
+
+// Keyboard Events lecture
+// .keydown() - activates when a key is pressed and activates on all keys
+// .keypress() - activates when a key is pressed and does not work with modifier keys (esc.)
+// .keyup() - activates when a key is release and works with all keys
+
+
+//$('document').ready(function () {
+//    $('#textfield').keyup(function () {
+//        alert('.keydown() event fired');
+//    });
+//});
+
+
+// the .on() is best used with AJAX
+
+//$('document').ready(function () {
+//    $('#textfield').on('keyup',function (){
+//      alert('.keyup() event fired');
+//    });
+
+//  $('#textfield').off('keyup');
+//  });
+
+
+
+})();
